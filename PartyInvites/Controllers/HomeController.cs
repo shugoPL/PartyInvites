65ef5c7a31efd8cc6dd603eccbfe67bae1f226a2
Maciej Góra <maciej.gora@outlook.com>
2018-08-11 14:35:20 +0200

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            
+            if (ModelState.IsValid) //Sprawdzenie czy wszystkie wymagane dane zostały wypełnione
+            {
+                //Do zrobienia: wyślij wiadomość o organizatora przyjęcia
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać swoje imię i nazwosko!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać swój adres e-mail")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Proszę podać prawidłowy adres e-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać numer telefonu!")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Proszę określić, czy weźmiesz udział")]
+        public bool? WillAttend { get; set; }
+    }
+}
